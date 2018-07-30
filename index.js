b6f0d21caf78b8dd77346f24925545df00272399
Lee Simon <shinoblee@gmail.com>
2018-07-30 19:22:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var uppercase = "HELLO";
-  uppercase.toUpperCase() === uppercase;
-  var lowercase = 'hello!';
-  lowercase.toLowerCase() === lowercase;
-  return string;
+  if (string === string.toLowerCase())
+  {
+    return "I can\'t hear you!"
+  } else if (string === string.toUpperCase()){
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.")
+  return "I love you, too."
 }
