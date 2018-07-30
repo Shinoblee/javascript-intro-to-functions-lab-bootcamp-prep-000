7fb9dfaccbc47ff24127cd3fa45173852a7ce5f7
Lee Simon <shinoblee@gmail.com>
2018-07-30 18:51:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+  return string.toUpperCase();
+}
+
+function whisper(string){
+  return string.toLowerCase();
+}
+
+function logShout(string){
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string){
+  let uppercase = "HELLO!";
+  uppercase.toUpperCase === uppercase;
+  return "YES INDEED!";
+}
+
+function sayHiToGrandma(string){
+  let lowercase = "HELLO!";
+  lowercase.toLowerCase === lowercase;
+  return "I can\'t hear you!";
+}
